59928ad307868f1c4c3779a99f0b8ed0a01f9c5a
RodionSavenko <savenko662@gmail.com>
2024-03-16 08:44:15 +0100

    fix

--- webapp-project-kiss/src/project/index.ts
+++ webapp-project-kiss/src/project/index.ts
@@ -85,6 +85,9 @@ function editButtonEvent(e: HTMLElement) {
     descriptionInput!.parentNode!.appendChild(projectDescription!);
     nameInput!.parentNode!.removeChild(nameInput!);
     descriptionInput!.parentNode!.removeChild(descriptionInput!);
+    parentNode.removeChild(btnSave);
+    parentNode.removeChild(btnCancel);
+    parentNode.appendChild(e);
     //location.reload();
   });
   parentNode.appendChild(btnSave);
