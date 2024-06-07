a80ba72a1e58361264cb577f1240b5de19e8eb82
RodionSavenko <savenko662@gmail.com>
2024-06-07 11:03:13 +0200

    Project story init

---
+++ webapp-react/src/components/project/ProjectStoryCreateDialog.tsx
@@ -0,0 +1,61 @@
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+
+type ProjectCreateDialogProps = {
+  onClose: () => void;
+  onCreate: (name: string, description: string) => void;
+};
+
+const ProjectStoryCreateDialog = ({
+  onClose,
+  onCreate,
+}: ProjectCreateDialogProps) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <Dialog open onClose={onClose}>
+      <DialogTitle>Project story create</DialogTitle>
+      <DialogContent>
+        <Stack gap="15px" width="500px" sx={{ marginTop: "5px" }}>
+          <TextField
+            label="Project name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value.trim());
+            }}
+            size="small"
+            required
+          />
+          <TextField
+            label="Project description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value.trim())}
+            size="small"
+            required
+          />
+        </Stack>
+      </DialogContent>
+      <DialogContent>
+        <Stack direction="row" justifyContent="space-between">
+          <Button onClick={onClose}>Close</Button>
+          <Button
+            onClick={() => onCreate(name, description)}
+            variant="contained"
+            disabled={!name || !description}
+          >
+            Create
+          </Button>
+        </Stack>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ProjectStoryCreateDialog;
--- webapp-react/src/pages/project/ProjectStories.tsx
+++ webapp-react/src/pages/project/ProjectStories.tsx
@@ -1,5 +1,40 @@
+import { Box, Button, Stack } from "@mui/material";
+import { useState } from "react";
+import ProjectStoryCreateDialog from "../../components/project/ProjectStoryCreateDialog";
+
 const ProjectStories = () => {
-  return <></>;
+  const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false);
+
+  const handleCreateDialogClose = () => {
+    setIsCreateDialogOpen(false);
+  };
+
+  const handleCreateDialogOpen = () => {
+    setIsCreateDialogOpen(true);
+  };
+
+  const handleCreateDialogCreate = () => {};
+  return (
+    <Box>
+      <Stack>Tabel</Stack>
+      <Stack>
+        <Button
+          sx={{ width: "150px", margin: "20px 0 0 10px" }}
+          variant="contained"
+          size="small"
+          onClick={handleCreateDialogOpen}
+        >
+          Create project
+        </Button>
+      </Stack>
+      {isCreateDialogOpen && (
+        <ProjectStoryCreateDialog
+          onClose={handleCreateDialogClose}
+          onCreate={handleCreateDialogCreate}
+        />
+      )}
+    </Box>
+  );
 };
 
 export default ProjectStories;
