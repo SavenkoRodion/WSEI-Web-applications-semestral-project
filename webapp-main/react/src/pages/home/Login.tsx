570bac73908379cc66c3ab0520c8501db1a81c6e
Rodion Savenko <rodion.savenko@microsoft.wsei.edu.pl>
2024-07-13 12:22:59 +0200

    ui hotfix

--- webapp-main/react/src/pages/home/Login.tsx
+++ webapp-main/react/src/pages/home/Login.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, OutlinedInput, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  OutlinedInput,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 import UserRepository from "../../repository/backend/UserRepository";
 import { useNavigate } from "react-router-dom";
@@ -11,23 +18,46 @@ const Login = () => {
   const navigate = useNavigate();
 
   return (
-    <Box sx={{padding: "10px"}}>
-      <TextField type={"outlined"} value={login} onChange={(e) => setLogin(e.target.value)} label="Login" />
-      <TextField
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        type={"password"}
-        label="Password"
-      />
-      <Button
-        onClick={async () => {
-          const response = await userRepository.authorize(login, password);
-          if (!response) setError("Failed to login");
-          else navigate("/");
-        }}
-      >
-        Send
-      </Button>
+    <Box
+      sx={{
+        padding: "15px",
+        width: "500px",
+        margin: "0 auto",
+        gap: "5px",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Stack>
+        <TextField
+          type={"outlined"}
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+          label="Login"
+          size="small"
+        />
+      </Stack>
+      <Stack>
+        <TextField
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type={"password"}
+          label="Password"
+          size="small"
+        />
+      </Stack>
+      <Stack>
+        <Button
+          onClick={async () => {
+            const response = await userRepository.authorize(login, password);
+            if (!response) setError("Failed to login");
+            else navigate("/");
+          }}
+          variant="contained"
+        >
+          Send
+        </Button>
+      </Stack>
       {!error ? (
         <Stack>
           <Typography>{error}</Typography>
